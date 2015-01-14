68229f7727472f259a4943172ab45355cb2cfd7b
Keldrif Darkflame <keldrif@hotmail.com>
2015-01-14 08:36:38 -0600

    initial commit

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,19 @@
+class PageController < ApplicationController
+  def contact
+  end
+
+  def about
+  end
+
+  def gallery
+  end
+
+  def home
+  end
+
+  def brands
+  end
+
+  def specialty
+  end
+end
